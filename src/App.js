35143abea31456b66b46b27eae62527d44188209
user-nexus <henrique.p.lima0@gmail.com>
2022-07-18 09:50:25 -0300

    Function Components and State examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactDOM } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Welcome name="Henrique" /> */}
+
+      <Clock />
     </div>
   );
 }
+
+// Function Component example
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+// State example
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, Henrique</h1>
+//       <h2>It is {props.date.toLocalTimeString()}.</h2>
+//     </div>
+//   )
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, Henrique</h1>
+        <h2>It is {this.props.date.toLocalTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Clock />);
 
 export default App;
