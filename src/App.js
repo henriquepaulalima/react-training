e93923c846b1f3cd33d5de71ad5d5c424f1556d0
user-nexus <henrique.p.lima0@gmail.com>
2022-07-19 08:57:17 -0300

    Add examples of List and NumberList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import NumberList from "./Base_Concepts_2/NumberList";
 
 function App() {
   return (
     <div>
+      <NumberList numbers={[1, 2, 3, 4, 5, 6]} />
     </div>
   );
 }
---
+++ src/Base_Concepts_2/List.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class List extends Component {
+  render() {
+    const numbers = [1, 2, 3, 4, 5];
+    const listItems = numbers.map((number) => <li>{number}</li>);
+
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+}
+
+export default List;
